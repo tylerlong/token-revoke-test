e932a3af02458a92efe83a44f4e6f9fdde723b35
Tyler Liu <tyler4long@gmail.com>
2024-03-29 10:57:03 -0700

    simplify code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import RingCentral from '@rc-ex/core';
-import waitFor from 'wait-for-async';
 
 const rc = new RingCentral({
   server: process.env.RINGCENTRAL_SERVER_URL,
@@ -11,24 +10,19 @@ const main = async () => {
   await rc.authorize({
     jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
   });
+  // save the old token
   const token = rc.token;
 
-  // for (let i = 0; i < 6; i++) {
-  //   await rc.authorize({
-  //     jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
-  //   });
-  //   console.log(i);
-  //   await waitFor({ interval: 2000 });
-  // }
-
-  // await rc.revoke();
-
+  // 1st refreshment
   await rc.refresh();
+
+  // an arbitrary API call
   await rc.restapi().account().extension().get();
+
+  // restore the old token
   rc.token = token;
-  // await waitFor({ interval: 2000 });
+
+  // refresh the old token again
   await rc.refresh();
-  // const r = await rc.restapi().account().extension().get();
-  // console.log(r);
 };
 main();
